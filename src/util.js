8c0585c9107af11e4f18f14e1b4f7400946b8e6b
Vitor Paixão <vitornvpaixao@gmail.com>
2022-05-19 12:21:21 +0100

    feat(convert-projection): add logic to convert projections to epsg 4326

--- src/util.js
+++ src/util.js
@@ -1,4 +1,5 @@
 import { TILE_SOURCE_TEMPLATE, STAC_PROXY_URL, TILE_PROXY_URL } from './config';
+import proj4 from 'proj4-fully-loaded'
 
 export const getProxiedUri = (uri) => {
   // If we are proxying a STAC Catalog, replace any URI with the proxied address.
@@ -25,4 +26,29 @@ const getTileProxiedUri = (uri) => {
 export const getTileSource = (assetHref) => {
   const proxiedUri = getTileProxiedUri(assetHref);
   return TILE_SOURCE_TEMPLATE.replace("{ASSET_HREF}", proxiedUri);
+}
+
+export const convertCoordinatesToEpsg4326 = (stacObj) => {
+  if (!!stacObj && !!stacObj['crs']) {
+    const stacEpsg = stacObj.crs.init.toUpperCase();
+
+    if (!!stacObj['geometry'] && stacObj['type'] === 'Feature' && !stacObj.crs['converted']) {
+      const geoCoordinates = stacObj.geometry.coordinates[0];
+      const coordinatesConverted = geoCoordinates.map((elem) => {
+        return proj4(`${stacEpsg}`, "EPSG:4326", [elem[0], elem[1]])
+      });
+
+      stacObj.crs.converted = true;
+      stacObj.geometry.coordinates[0] = coordinatesConverted;
+
+    } else if (!!stacObj['extent'] && stacObj['type'] === 'Collection' && !stacObj.crs['converted']) {
+      const bboxdCoordinates = stacObj.extent.spatial.bbox[0];
+      const coordinatesConverted = [...proj4(`${stacEpsg}`, "EPSG:4326", [bboxdCoordinates[0], bboxdCoordinates[1]]), ...proj4(`${stacEpsg}`, "EPSG:4326", [bboxdCoordinates[2], bboxdCoordinates[3]])];
+      stacObj.crs.converted = true;
+      stacObj.extent.spatial.bbox[0] = coordinatesConverted;
+
+    }
+  }
+
+  return stacObj;
 }
